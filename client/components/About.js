f16251253e9ae3200430e2418d4eb9939db9f52d
Celia Macrae <celiamacrae@gmail.com>
2020-05-05 16:54:36 -0400

    updated about me section content

--- client/components/About.js
+++ client/components/About.js
@@ -10,12 +10,13 @@ const About = () => {
       <h3 id="aboutme">
         My name is Celia Macrae. I am a full-stack web developer based in New
         York, NY. I recently graduated from Duke University (2019) with a
-        Bachelors Degree in Computer Science and a minor in Sociology,
-        particularly focused on Educational Sociology. After my graduation, I
+        Bachelor's Degree in Computer Science and a minor in Sociology,
+        particularly focused on Educational Sociology. After graduating, I
         completed the Grace Hopper Program at Fullstack Academy to sharpen my
-        tecnical skills and practice working on team projects. I am currently
-        working as a Software Engineering Teaching Fellow at Fullstack Academy
-        and seeking fulltime employment as a Software Engineer.
+        technical skills (NERD Stack) and gain experience working on agile
+        teams. I am currently working as a Software Engineering Teaching Fellow
+        at Fullstack Academy and seeking fulltime employment as a Software
+        Engineer.
       </h3>
     </div>
   )
--- public/bundle.js
+++ public/bundle.js
@@ -107,7 +107,7 @@ var About = function About() {
     src: "https://i.imgur.com/Zaxi80t.jpg"
   }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
     id: "aboutme"
-  }, "My name is Celia Macrae. I am a full-stack web developer based in New York, NY. I recently graduated from Duke University (2019) with a Bachelors Degree in Computer Science and a minor in Sociology, particularly focused on Educational Sociology. After my graduation, I completed the Grace Hopper Program at Fullstack Academy to sharpen my tecnical skills and practice working on team projects. I am currently working as a Software Engineering Teaching Fellow at Fullstack Academy and seeking fulltime employment as a Software Engineer."));
+  }, "My name is Celia Macrae. I am a full-stack web developer based in New York, NY. I recently graduated from Duke University (2019) with a Bachelor's Degree in Computer Science and a minor in Sociology, particularly focused on Educational Sociology. After graduating, I completed the Grace Hopper Program at Fullstack Academy to sharpen my technical skills (NERD Stack) and gain experience working on agile teams. I am currently working as a Software Engineering Teaching Fellow at Fullstack Academy and seeking fulltime employment as a Software Engineer."));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (About);
@@ -850,7 +850,7 @@ var About = function About() {
     src: "https://i.imgur.com/Zaxi80t.jpg"
   }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
     id: "aboutme"
-  }, "My name is Celia Macrae. I am a full-stack web developer based in New York, NY. I recently graduated from Duke University (2019) with a Bachelors Degree in Computer Science and a minor in Sociology, particularly focused on Educational Sociology. After my graduation, I completed the Grace Hopper Program at Fullstack Academy to sharpen my tecnical skills and practice working on team projects. I am currently working as a Software Engineering Teaching Fellow at Fullstack Academy and seeking fulltime employment as a Software Engineer."));
+  }, "My name is Celia Macrae. I am a full-stack web developer based in New York, NY. I recently graduated from Duke University (2019) with a Bachelor's Degree in Computer Science and a minor in Sociology, particularly focused on Educational Sociology. After graduating, I completed the Grace Hopper Program at Fullstack Academy to sharpen my technical skills (NERD Stack) and gain experience working on agile teams. I am currently working as a Software Engineering Teaching Fellow at Fullstack Academy and seeking fulltime employment as a Software Engineer."));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (About);
